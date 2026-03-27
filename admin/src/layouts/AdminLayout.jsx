b6af722cfc362cd5d64e1dce08c5b8a017885749
Mohammad Hamza <mhdkh444@gmail.com>
2026-03-27 20:20:09 +0530

    mini update

--- admin/src/layouts/AdminLayout.jsx
+++ admin/src/layouts/AdminLayout.jsx
@@ -6,25 +6,42 @@ import axios from 'axios';
 
 const AdminLayout = () => {
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(true); 
+
   const verifyAdmin = async () => {
     try {
-      const response = await axios.get(`${import.meta.env.VITE_BACKEND_API_URL}/api/admin/verify`, {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem("adminToken")}`,
-        },
-      });
-      console.log(response);
+      const response = await axios.get(
+        `${import.meta.env.VITE_BACKEND_API_URL}/api/admin/verify`,
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("adminToken")}`,
+          },
+        }
+      );
+
       if (!response.data.success) {
         navigate("/admin/login");
       }
     } catch (error) {
       console.error("Login Error", error);
       navigate("/admin/login");
+    } finally {
+      setLoading(false);
     }
   };
+
   useEffect(() => {
     verifyAdmin();
   }, []);
+
+  if (loading) {
+    return (
+      <div className="flex items-center justify-center h-screen text-xl font-semibold">
+        Loading...
+      </div>
+    );
+  }
+
   return (
     <div className="flex min-h-screen bg-gray-50">
       <Sidebar />
